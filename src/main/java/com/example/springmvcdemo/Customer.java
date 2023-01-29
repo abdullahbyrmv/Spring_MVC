7f2f810e2b71ec87670486464c6eca4edaf7d92f
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2023-01-29 09:22:04 +0400

    Spring MVC

--- src/main/java/com/example/springmvcdemo/Customer.java
+++ src/main/java/com/example/springmvcdemo/Customer.java
@@ -1,5 +1,7 @@
 package com.example.springmvcdemo;
 
+import validation.CourseCode;
+
 import javax.validation.constraints.*;
 
 public class Customer {
@@ -17,6 +19,17 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "course code must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
